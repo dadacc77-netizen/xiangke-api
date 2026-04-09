64ba48e961295221d5dd45e4031541212a61a972
dadacc77-netizen <dadacc77@gmail.com>
2026-04-09 18:33:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); // 🔥 這行一定要有
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
